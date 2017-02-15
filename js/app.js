dae843e6672f4637cce08320640f1ea5232d1b5e
Alex Perez <redflare@gmail.com>
2017-02-14 19:46:49 -0800

    Removed ocitcons for later release, added site description, changed code selector font color and background

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@ var main = function() {
 // selects all <a> elements that don't begin with a hash or contain the root, and adds target="_blank"
   $('a:not([href^="#"], [href^="/"])').attr('target', '_blank');
 
-  $('.gtfs-tag').click(function(e) {
-
-    // To be used later in copy-to-clipboard functionality
-    // var link = location.protocol + "//" + location.host + location.pathname + "#" + e.target.id;
-
-    window.location.href = "#" + e.currentTarget.id;
-
-    console.log(e.currentTarget.id);
-
-  });
+  // $('.gtfs-tag').click(function(e) {
+  //
+  //   // To be used later in copy-to-clipboard functionality
+  //   // var link = location.protocol + "//" + location.host + location.pathname + "#" + e.target.id;
+  //
+  //   window.location.href = "#" + e.currentTarget.id;
+  //
+  //   console.log(e.currentTarget.id);
+  //
+  // });
 
 }
 
